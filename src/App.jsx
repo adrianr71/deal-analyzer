c4fc2a982214d11f657ccea697a55c63e9bdefad
adrianr71 <amandry@hotmail.com>
2026-05-23 12:33:19 -0400

    Updated Footer links for Terms, Privacy, and Disclaimer pagers

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InvestorV1 from "./pages/InvestorV1";
 import AgentV2 from "./pages/AgentV2";
 
+import Terms from "./pages/Terms";
+import Privacy from "./pages/Privacy";
+import Disclaimer from "./pages/Disclaimer";
+
 export default function App() {
   return (
     <BrowserRouter>
