011e9b453e3d1d4c9deaedf1bcc749026bf63f22
adrianr71 <amandry@hotmail.com>
2026-07-09 22:21:29 -0400

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -16,6 +16,9 @@ export default async function handler(req, res) {
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
       payment_method_types: ["card"],
+      automatic_tax: {
+      enabled: true,
+      },
       line_items: [
         {
           price: process.env.STRIPE_PRICE_ID,
